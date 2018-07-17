f7b2bf8ac7aaf39587d1ccc6d7aab6a0a2c6bcaf
Sreehari.KV <Sreeharikv112@gmail.com>
2018-07-17 22:30:46 +0530

    WORKS FOR CONTINUOUS STRIKES.
    DIDINT CHECKED WITH MIX OF STRIKES AND SPARES

--- src/one/bow/MainManager.java
+++ src/one/bow/MainManager.java
@@ -7,27 +7,136 @@ import java.util.Map;
 
 public class MainManager {
 
-	ArrayList<HashMap<Integer, Integer>> strikePointsWaitList = new ArrayList<HashMap<Integer, Integer>>();
-	int spare=-1;
-	int strike=-2;
+	//ArrayList<HashMap<Integer, Integer>> strikePointsWaitList = new ArrayList<HashMap<Integer, Integer>>();
+	
+	ArrayList<WaitingObject> strikeWaitingList = new ArrayList<WaitingObject>();
+	
+	int spare=20;
+	int strike=10;
+	int waitStrike=-3;
+	int strikeBuddy=-4;
 	
 	boolean shouldWaitForNextOne=false;
 	int spareWaitIndex=-1;
 	int spareScoreLast=-1;
 	
-	boolean shouldWaitForNextTwo=false;
+	//boolean shouldWaitForNextTwo=false;
+	
 	int strikeWaitIndex=-1;
 	int strikeScoreLast=-1;
-	int waitStrike=-1;
-	int strikeBuddy=-1;
+	
 			
-	int[][] input = new int[][]{{1,2},{strike,strikeBuddy},{2,4},{strike,strikeBuddy},{4,4}};
+	int[][] input = new int[][]{{1,2},{strike,strikeBuddy},{strike,strikeBuddy},{6,1}};
 	int [][]output=new int[input.length][1];
 	int localSum=-1;
 	
 	public MainManager() {
-				
+						
+		//WORKS FOR CONTINUOUS STRIKES. DIDINT CHECKED WITH MIX OF STRIKES AND SPARES
 		for (int row = 0; row < input.length; row ++){
+			localSum=0;
+			for (int col = 0; col < 2; col++){
+					
+					int currentVal = input[row][col];
+        			System.out.println (currentVal);
+        				
+        				if(currentVal == strikeBuddy){
+        					continue;
+        				}
+        				
+        				//Iterate over @strikeWaitingList
+        				if(strikeWaitingList.size()>0){
+                			
+                			for (int i = 0; i < strikeWaitingList.size(); i++) {
+								WaitingObject waitingObject = strikeWaitingList.get(i);
+								//Check if waiting object's inputIndex is  
+								int localRow = row;
+								if((waitingObject.inputIndex == (localRow-1)) || (waitingObject.inputIndex == (localRow-2))){
+									if(waitingObject.iterationCount!=0 && waitingObject.iterationCount!=-1){
+										
+										waitingObject.liveScore = waitingObject.liveScore+currentVal;
+										waitingObject.iterationCount = --waitingObject.iterationCount;//update iteration count 
+										if(waitingObject.iterationCount==0){
+											output[waitingObject.inputIndex][0]=waitingObject.liveScore;//update the score
+											waitingObject.iterationCount=-1;
+											//strikeWaitingList.remove(i);
+										}
+									}
+								}
+							}
+        			}
+        			
+		        	if(shouldWaitForNextOne){
+                		spareScoreLast = spareScoreLast+currentVal;
+                		shouldWaitForNextOne=false;
+                		//put value to last index
+                		output[spareWaitIndex][0]= spareScoreLast;
+                		//reset the indexes
+                		spareScoreLast=-1;
+                		spareWaitIndex=-1;   
+                		localSum=0;
+                	}
+                	if(currentVal==spare){
+	                		shouldWaitForNextOne=true;
+	                		spareScoreLast=10;
+	                		spareWaitIndex=row;
+	                }
+                	else if(currentVal ==strike){
+                			/*shouldWaitForNextTwo=true;*/
+                			waitStrike=2;
+                			strikeScoreLast=10;
+                			strikeWaitIndex=row;
+                				
+                			if(strikeWaitingList.size()>0){
+                			boolean isValueAvailable=false;
+                			for (int i = 0; i < strikeWaitingList.size(); i++) {
+								WaitingObject waitingObject = strikeWaitingList.get(i);
+								if(waitingObject.inputIndex==row){
+									isValueAvailable=true;
+									waitingObject.liveScore = waitingObject.liveScore+strikeScoreLast;//will this update ?
+								}
+							}
+                				//Iterated over array, element with index not available. So create new and add to list
+	                			if(!isValueAvailable){
+	                				WaitingObject waitingObject =new WaitingObject();
+	                				waitingObject.inputIndex=row;
+	                				waitingObject.iterationCount=2;
+	                				waitingObject.liveScore=10;
+	                				strikeWaitingList.add(waitingObject);
+	                			}
+                			}else{
+                				WaitingObject waitingObject =new WaitingObject();
+                				waitingObject.inputIndex=row;
+                				waitingObject.iterationCount=2;
+                				waitingObject.liveScore=10;
+                				strikeWaitingList.add(waitingObject);
+                			}
+                	}
+                	else{
+	                	localSum=localSum+currentVal;
+	                }
+                	if(col==1){
+                		
+                		if(spareScoreLast!=-1 && (false==shouldWaitForNextOne)){
+                			output[row][0]=spareScoreLast;
+                    		localSum=0;	
+                    		spareWaitIndex = row;
+                		}else if(shouldWaitForNextOne){
+                			//don't add sum, keep values in waiting list only
+                			continue;  			
+                		}
+                		else{
+                			output[row][0]=localSum;
+                			localSum=0;	
+                		}
+                	}
+		}
+		
+		/*
+		 * WORKS FOR INTERMEDIARY STRIKES.
+		 * WORKS FOR STRIKES , SPARES COMBINATION AS WELL
+		 * NOT WORKING FOR CONTINUOUS STRIKES.
+		 * for (int row = 0; row < input.length; row ++){
 			localSum=0;
 			for (int col = 0; col < 2; col++){
 					
@@ -115,13 +224,45 @@ public class MainManager {
                 		}
                 	}
             }
-		}
+		}*/
 		
 		//Print the saved elements in output array
+		
+	}
 		printOutput();
 	}
 	
-	private int isValueAvailable(int indexToSearch){
+private WaitingObject isValueAvailable(int indexToSearch){
+		
+		WaitingObject valueFound=null;
+		for (int i = 0; i < strikeWaitingList.size(); i++) {
+			WaitingObject waitingObject = strikeWaitingList.get(i);
+			if(waitingObject.inputIndex==indexToSearch){
+				valueFound=waitingObject;
+				break;
+			}
+		}		
+		return valueFound;
+	}
+	
+	/*private void updateExistingValue(int index,HashMap<Integer, Integer> newValue) {
+		
+		for (int i = 0; i < strikeWaitingList.size(); i++) {
+			HashMap<Integer, Integer> hashMap = strikePointsWaitList.get(i);
+			for (Map.Entry<Integer, Integer> entry : hashMap.entrySet())
+			{
+			    System.out.println(entry.getKey() + " || " + entry.getValue());
+			    if(entry.getKey()==index){
+			    	strikePointsWaitList.set(i, newValue);
+			    	break;
+			    }else{
+			    	continue;
+			    }
+			}
+		}
+	}*/
+	
+	/*private int isValueAvailable(int indexToSearch){
 		
 		int valueFound=-1;
 		for (int i = 0; i < strikePointsWaitList.size(); i++) {
@@ -155,6 +296,14 @@ public class MainManager {
 			    }
 			}
 		}
+	}*/
+	
+	private class WaitingObject {
+		
+		public int inputIndex;
+		public int liveScore;
+		public int iterationCount;
+		
 	}
 
 	public static void main(String[] args) {
